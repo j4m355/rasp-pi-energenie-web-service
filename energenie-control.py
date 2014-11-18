752e8c2e4ffa0868451b30cbba8a3dafc6b4f1f0
James <e@mailjames.com>
2014-11-18 14:37:39 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -9,10 +9,10 @@ def pi_switch(switch,status):
     print switch
     print "status:"
     print status
-    english = ""
+    english == ""
     if status = 1:
         english = " on"
-    if status = 0:
+    if status == 0:
         english = " off"
     s = "Swith " + switch + " has been switched " + english
     return s
