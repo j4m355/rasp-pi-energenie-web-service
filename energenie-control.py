e2ec0cd06035129d8ad51f168c7f8c82fa18d860
James <e@mailjames.com>
2014-11-18 13:00:12 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -1,6 +1,9 @@
 from energenie import switch_on, switch_off
+from werkzeug.wrappers import Request, Response
 
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['Hello World!']
+    request = Request(environ)
+    text = 'Hello %s!' % request.args.get('name', 'World')
+    response = Response(text, mimetype='text/plain')
+    return response(environ, start_response)
 
