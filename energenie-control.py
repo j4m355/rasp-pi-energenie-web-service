c2613059aebc2c1dc023a8bef7e78c450e13c9b8
James <e@mailjames.com>
2014-11-18 13:09:39 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -1,9 +1,30 @@
 from energenie import switch_on, switch_off
-from werkzeug.wrappers import Request, Response
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-def application(environ, start_response):
-    request = Request(environ)
-    text = 'Hello %s!' % request.args.get('name', 'World')
-    response = Response(text, mimetype='text/plain')
-    return response(environ, start_response)
+PORT_NUMBER = 8080
 
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+	
+	#Handler for the GET requests
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
