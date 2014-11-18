931247805de276e56274eae472c761d9de3125f1
James <e@mailjames.com>
2014-11-18 14:23:21 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -1,10 +1,11 @@
 from wsgiref.validate import validator
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print request.data
     return "Hello, World!"
 
 if __name__ == '__main__':
