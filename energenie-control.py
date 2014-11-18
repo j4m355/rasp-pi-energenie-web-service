c95c72b18076ecb5d74170cf61677009cf03b093
James <e@mailjames.com>
2014-11-18 13:52:03 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -1,20 +1,22 @@
-from wsgiref.util import setup_testing_defaults
+from wsgiref.validate import validator
 from wsgiref.simple_server import make_server
 
-# A relatively simple WSGI application. It's going to print out the
-# environment dictionary after being updated by setup_testing_defaults
+# Our callable object which is intentionally not compliant to the
+# standard, so the validator is going to break
 def simple_app(environ, start_response):
-    setup_testing_defaults(environ)
-
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
-
+    status = '200 OK' # HTTP Status
+    headers = [('Content-type', 'text/plain')] # HTTP Headers
     start_response(status, headers)
 
+    # This is going to break because we need to return a list, and
+    # the validator is going to inform us
     ret = ["%s: %s\n" % (key, value)
            for key, value in environ.iteritems()]
     return ret
 
-httpd = make_server('', 8000, simple_app)
-print "Serving on port 8000..."
+# This is the application wrapped in a validator
+validator_app = validator(simple_app)
+
+httpd = make_server('', 8000, validator_app)
+print "Listening on port 8000...."
 httpd.serve_forever()
