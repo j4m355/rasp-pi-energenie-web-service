df79dcb96e813dcecb8b08dbc9842eeb7ca97317
James <e@mailjames.com>
2014-11-18 13:14:19 +0000

    query

--- energenie-control.py
+++ energenie-control.py
@@ -16,6 +16,9 @@ class myHandler(BaseHTTPRequestHandler):
 		self.wfile.write("Hello World !")
 		return
 	def do_POST(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
 		content_len = int(self.headers.getheader('content-length', 0))
 		post_body = self.rfile.read(content_len)
 		self.wfile.write(post_body)
