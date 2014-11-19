de011d7ea87244ec90a989235b75b473af35aea4
James <e@mailjames.com>
2014-11-19 14:07:32 +0000

    better

--- energenie-server.py
+++ energenie-server.py
@@ -31,7 +31,7 @@ def json_post():
     body = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
     print body.PlugState
     print body.PlugNumber
-    if body.PlugState == "true":
+    if body.PlugState:
         print "inside true"
         switch_on(int(body.PlugNumber))
     else:
